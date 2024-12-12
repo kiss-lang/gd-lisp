3a684c8595e734df40b6ac51a09858158463b665
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-12-12 09:39:44 -0600

    Github actions testing

--- test.sh
+++ test.sh
@@ -1,14 +1,21 @@
 #! /bin/bash
 
+args="--headless --script"
+
+app=$(which godot)
+if [ -z "$app" ]; then
+    app=$(which Godot)
+fi
+
 if [ -z "$cases" ]; then
     for script in "src/gd_lisp/test/*.gd"; do
         haxe test.hxml --run gd_lisp.lib.Main $script
-        Godot --headless --script $script
+        "$app" $args $script
     done
 else
     for case in "$cases"; do
         script=src/gd_lisp/test/${case}.gd
         haxe test.hxml --run gd_lisp.lib.Main $script 
-        Godot --headless --script $script
+        "$app" $args $script
     done
 fi
